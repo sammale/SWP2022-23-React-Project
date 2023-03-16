558cdc5e08f787c48d06bde271c8e46caf2353cd
Julian Sammer <julian.sammer@student.htldornbirn.at>
2023-03-16 11:29:33 +0100

    first steps

--- a-million-times/src/App.js
+++ a-million-times/src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Button_Zeiger from './components/Zeiger';
+import UhrEinzeln from './components/Zeiger';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    function rotation() {
+      document.querySelector("#rotate").addEventListener('click', function() {
+        document.querySelector("#sample").classList.add('rotating')
+      });
+    }
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
       
-          <Button_Zeiger name="David der Lutscher"/>
+          <UhrEinzeln>
+          </UhrEinzeln>
       </header>
     </div>
   );
--- a-million-times/src/components/Zeiger.js
+++ a-million-times/src/components/Zeiger.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from './Zeiger.module.css'
 
-export default function Button_Zeiger() {
+export default function UhrEinzeln() {
   return (
-    <div className={styles.main}>{}</div>
-    
+      <div className={styles.main}>{
+        <div id={styles.rotate} className={'zeiger rotating'}>{}</div>
+      }</div>
   )
 }
+
+
