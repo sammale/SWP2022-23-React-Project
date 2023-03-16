40a2c2750fd5847e698fd0859d41f5efc7c6dc6d
Julian Sammer <julian.sammer@student.htldornbirn.at>
2023-03-16 10:06:56 +0100

    milliontimes

---
+++ a-million-times/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Button_Zeiger from './components/Zeiger';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+          <Button_Zeiger name="David der Lutscher"/>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ a-million-times/src/components/Zeiger.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Zeiger.module.css'
+
+export default function Button_Zeiger() {
+  return (
+    <div className={styles.main}>{}</div>
+    
+  )
+}
