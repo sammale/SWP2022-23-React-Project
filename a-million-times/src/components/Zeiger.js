68872a730d3ba9b5aae6fb111b96cd0dfdcaed1a
Julian Sammer <julian.sammer@student.htldornbirn.at>
2023-03-17 16:40:05 +0100

    rotation

--- a-million-times/src/components/Zeiger.js
+++ a-million-times/src/components/Zeiger.js
@@ -4,7 +4,7 @@ import styles from './Zeiger.module.css'
 export default function UhrEinzeln() {
   return (
       <div className={styles.main}>{
-        <div id={styles.rotate} className={'zeiger rotating'}>{}</div>
+        <div className={styles.zeiger}>{}</div>
       }</div>
   )
 }
